6dbde874f1de5f54c1627b44192c067d71546763
tanudawande04 <tanudawande54@gmail.com>
2025-07-06 23:52:10 +0530

    added  new  function or methods

--- ToDoLIST/script.js
+++ ToDoLIST/script.js
@@ -1,26 +1,42 @@
-const TodoInput = document.getElementById("todo-input");
-const AddTaskBtn = document.getElementById("add-task-btn");
-const TodoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+    const TodoInput = document.getElementById("todo-input");
+    const AddTaskBtn = document.getElementById("add-task-btn");
+    const TodoList = document.getElementById("todo-list");
 
 
-let tasks = []
-AddTaskBtn.addEventListener("click", () => {
-    const taskText = TodoInput.value.trim();
-    if (taskText === "") {
-        return;
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach((tasks) => {
+        renderTaska(tasks);
+    });
+
+    AddTaskBtn.addEventListener("click", () => {
+        const taskText = TodoInput.value.trim();
+        if (taskText === "") {
+            return;
+        }
+
+        const newTask = {
+            text: taskText,
+            id: Date.now(),
+            comleted: false,
+
+        };
+
+        tasks.push(newTask);
+        saveTasks();
+        TodoInput.value = "";
+        console.log(tasks);
+    });
+
+    function renderTaska(task) {
+        console.log(tasks);
+
     }
 
-    const newTask = {
-        text: taskText,
-        id: Date.now(),
-        comleted: false,
+    function saveTasks() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
 
     }
-
-    tasks.push(newTask);
-    TodoInput.value = "";
-
-
 
 
 });
